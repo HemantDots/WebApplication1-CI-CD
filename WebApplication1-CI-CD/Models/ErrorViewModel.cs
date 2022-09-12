a09a1158c80a9805fac438863367eb96384e5834
Hemant <hemant.sharma@dotsquares.com>
2022-09-12 07:30:43 +0530

    Add project files.

---
+++ WebApplication1-CI-CD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1_CI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
